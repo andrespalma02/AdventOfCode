206515af2b230401c27b63d0086299866dfd9a3c
Andres Palma <bryan.palma02@epn.edu.ec>
2024-07-22 20:24:56 -0500

    APA-refactor venv

--- Rosalind/Bioinformatics Stronghold/ORF/ORF.py
+++ Rosalind/Bioinformatics Stronghold/ORF/ORF.py
@@ -1,33 +1,112 @@
-from Bio.Seq import reverse_complement, Seq
+#!/usr/bin/env python
+# coding=utf-8
+
+# Open Reading Frames
+# ===================
+# 
+# Either strand of a DNA double helix can serve as the coding strand for RNA
+# transcription. Hence, a given DNA string implies six total reading frames, or
+# ways in which the same region of DNA can be translated into amino acids: three
+# reading frames result from reading the string itself, whereas three more
+# result from reading its reverse complement.
+# 
+# An open reading frame (ORF) is one which starts from the start codon and ends
+# by stop codon, without any other stop codons in between. Thus, a candidate
+# protein string is derived by translating an open reading frame into amino
+# acids until a stop codon is reached.
+# 
+# Given: A DNA string s of length at most 1 kbp.
+# 
+# Return: Every distinct candidate protein string that can be translated from
+# ORFs of s. Strings can be returned in any order.
+# 
+# Sample Dataset
+# --------------
+# AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG
+# 
+# Sample Output
+# -------------
+# MLLGSFRLIPKETLIQVAGSSPCNLS
+# M
+# MGMTPRLGLESLLE
+# MTPRLGLESLLE
 
 
-def subarreglo_compatible(arr):
-    inicio = -1
-    seq = ""
-    for i in range(0, len(arr) - 2, 3):
-        if arr[i:i + 3] == "ATG":
-            inicio = i
-            break
-    if inicio != -1:
-        sub_arr = arr[inicio:]
-        res = len(sub_arr) % 3
-        seq = sub_arr[:len(sub_arr) - res]
-    return Seq(seq)
+DNA_CODON_TABLE = {'TTT': 'F', 'CTT': 'L', 'ATT': 'I', 'GTT': 'V', 'TTC': 'F', 'CTC': 'L', 'ATC': 'I', 'GTC': 'V',
+                   'TTA': 'L', 'CTA': 'L', 'ATA': 'I', 'GTA': 'V', 'TTG': 'L', 'CTG': 'L', 'ATG': 'M', 'GTG': 'V',
+                   'TCT': 'S', 'CCT': 'P', 'ACT': 'T', 'GCT': 'A', 'TCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+                   'TCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A', 'TCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+                   'TAT': 'Y', 'CAT': 'H', 'AAT': 'N', 'GAT': 'D', 'TAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+                   'TAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E', 'TAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+                   'TGT': 'C', 'CGT': 'R', 'AGT': 'S', 'GGT': 'G', 'TGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+                   'TGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G', 'TGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'}
 
 
-seq = Seq("AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG")
-rc_seq = reverse_complement(seq)
-seqs = []
-proteins = set()
+def translate_codon(codon):
+    protein = None
+    if len(codon) == 3 and codon in DNA_CODON_TABLE:
+        protein = DNA_CODON_TABLE[codon]
+    return protein
 
-for i in range(3):
-    seqs.append(subarreglo_compatible(seq[i:]))
-    seqs.append(subarreglo_compatible(rc_seq[i:]))
 
-for seq in seqs:
-    translated_seq = seq.translate(to_stop=True)
-    if translated_seq != "":
-        proteins.add(translated_seq)
+def reverse_complement(dna):
+    lookup = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    return ''.join([lookup[c] for c in reversed(dna)])
 
-for protein in proteins:
-    print(protein)
+
+def possible_protein_strings(s):
+    results = []
+    indices = []
+
+    l = len(s)
+    for i in range(l):
+        protein = translate_codon(s[i:i + 3])
+        if protein and protein == 'M':
+            indices.append(i)
+
+    for i in indices:
+        found_stop = False
+        protein_string = ''
+
+        for j in range(i, l, 3):
+            protein = translate_codon(s[j:j + 3])
+
+            if not protein:
+                break
+
+            if protein == 'Stop':
+                found_stop = True
+                break
+
+            protein_string += protein
+
+        if found_stop:
+            results.append(protein_string)
+
+    return results
+
+
+if __name__ == "__main__":
+
+    small_dataset = ("CTGCGGAGCTTGGTATGGGCACTATTATGTGTAATCATGACTGGGACAAATAGTATACCCTTTAAGAAAGTGAGGGATTCGCAGTGGAACTAAGGG"
+                     "TAGCACTTTATCAGAGGCTCATAAAGATTGGTTGAGCAGGCCACGTCGCTCTTTGCCATAATTCGGGCGCGGTTCGCACTGTAAGCTCGTTCCTAA"
+                     "ATGACGTAACTTTGCTAATTACAGAGGATATTAGGAAGGGCCAAGAATTAGGGTATCACTCGCCGTATCCGTCGATAACCTATGGTGTCCTGGTTG"
+                     "AATGATACGTAGCAGAACATTAGCTCCTGCGATACGCTCAATGCACGCGGTAAGGTAATCCCCCCTTGGTCCATCCGCCATTTACTATATCATGCA"
+                     "CGAATGGAATGTATTTACGGCAAATGTAGAACTTGGTGACGGAGACGCGGATATGTATAGTACCTTTATAATTCAAACATAGCTATGTTTGAATTA"
+                     "TAAAGGTACTATACATATAACAGTGCTTGTTAGTCGGTTGCCTCTTTCATTCGGCTTCCAATCCTTAACCGGGTGGCCGTCTATGTGTTCGGCAAG"
+                     "CATGCAGTTCCACCGCAGTTCTCTAAGAGAGGGATACTCCTGCCAGCCGTCCCATCACTCGATCAAAGCAGACGATGGGTTGTGCGACTGTGTCTC"
+                     "TTGAAAGAATGAACTAATTTTCAGGCGAGGTTACCTAGTATAATGGCCGTATGCCACAACAAAAGAAGCAAAGCAGCCCCGGAGCCGATGCAGGGA"
+                     "TCTACTCGACAGGGACTCCGTATACGACAAGCCGATACCAACATGTATGGAGACGCAAGCCTCCCCTCCGAGGAGTCTTTCAGTCCCCATATTCAC"
+                     "CTATTCTCGTCGTCCACTTATAGCCAACAGCAGCCACTAGGACGATTACGGAGAATTGAGCTAGGCTC")
+    large_dataset = open('rosalind_orf.txt').read().strip()
+
+    test = True
+
+    if test:
+        dataset = small_dataset
+    else:
+        dataset = large_dataset
+
+    possible_a = possible_protein_strings(dataset)
+    possible_b = possible_protein_strings(reverse_complement(dataset))
+    print("\n".join(set(possible_a + possible_b)))
